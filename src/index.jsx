7d462396f2b9ed205488fcdbef374ec1d8da0c75
Pavlina Mullerova <Pauline@pavlinas-macbook-air.home>
2020-11-21 14:33:03 +0100

    pridani obrazku a stylu

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.html';
 
-render(
-  <>
-    <header>
-      <h1>React Starter</h1>
-    </header>
-    <main>
-      <p>Moje první React stránka</p>
-    </main>
-    <footer>Martin Podloucký</footer>
-  </>,
-  document.querySelector('#app'),
-);
+const App = () => {
+  return (
+    <div>
+      <header>
+        <h1>React Starter</h1>
+      </header>
+      <main>
+        <p>Moje první React stránka</p>
+      </main>
+    </div>
+  );
+};
+
+render(<App />, document.querySelector('#app'));
